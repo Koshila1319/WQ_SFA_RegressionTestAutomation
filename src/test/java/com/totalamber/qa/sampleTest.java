af8a82532432b2db73ba97f1dc610d4fb94db707
suranjith <suranjith.rodrigo@gmail.com>
2017-09-12 11:25:41 +0530

    removing un-wanted files

--- src/test/java/com/totalamber/qa/sampleTest.java
+++
@@ -1,7 +0,0 @@
-package com.totalamber.qa;
-
-/**
- * Created by s.rodrigo on 8/9/2017.
- */
-public class sampleTest {
-}
--- src/test/java/com/totalamber/qa/test1.java
+++
@@ -1,7 +0,0 @@
-package com.totalamber.qa;
-
-/**
- * Created by s.rodrigo on 8/14/2017.
- */
-public class test1 {
-}
