9d4b6a0d919f55e6856b48f9f5237b809aa29836
Shammi <s.wathsala@totalamber.com>
2017-09-28 17:48:46 +0530

    partially commiting to get master changes

--- src/main/java/com/totalamber/qa/page/webQuarters/WQClientRegCompletePage.java
+++ src/main/java/com/totalamber/qa/page/webQuarters/WQClientRegCompletePage.java
@@ -6,7 +6,7 @@ import com.totalamber.qa.data.UI.elements.webQuarters.wqCheckMailCatchEmailPage;
 import com.totalamber.qa.data.UI.elements.webQuarters.wqClientRegCompletePage;
 import org.openqa.selenium.WebDriver;
 
-import static com.totalamber.qa.data.UI.elements.webQuarters.wqCheckMailCatchEmailPage.WQ_CLIENT_SIGNUP_CONFIRMATION_EMAIL_VERIFICATION_LINK_XPATH;
+
 
 
 /**
